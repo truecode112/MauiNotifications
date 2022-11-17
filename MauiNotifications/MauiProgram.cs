591d637fc06a6132f5de5bfc9a794724ccbf4e53
Dan Popov <akiowebdev@gmail.com>
2022-11-17 18:16:14 +0900

    Integrate MudBlazor and changed subscriber page design.

--- MauiNotifications/MauiProgram.cs
+++ MauiNotifications/MauiProgram.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components.WebView.Maui;
 using MauiNotifications.Data;
+using MudBlazor.Services;
+using MudBlazor;
 
 namespace MauiNotifications;
 
@@ -16,8 +18,20 @@ public static class MauiProgram
 			});
 
 		builder.Services.AddMauiBlazorWebView();
-		#if DEBUG
-		builder.Services.AddBlazorWebViewDeveloperTools();
+		builder.Services.AddMudServices(config =>
+		{
+            config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomCenter;
+
+            config.SnackbarConfiguration.PreventDuplicates = true;
+            config.SnackbarConfiguration.NewestOnTop = true;
+            config.SnackbarConfiguration.ShowCloseIcon = true;
+            config.SnackbarConfiguration.VisibleStateDuration = 2000;
+            config.SnackbarConfiguration.HideTransitionDuration = 300;
+            config.SnackbarConfiguration.ShowTransitionDuration = 300;
+            config.SnackbarConfiguration.SnackbarVariant = Variant.Filled;
+        });
+#if DEBUG
+        builder.Services.AddBlazorWebViewDeveloperTools();
 #endif
 		
 		builder.Services.AddSingleton<WeatherForecastService>();
